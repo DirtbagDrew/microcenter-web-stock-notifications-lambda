08772df6b5431dcffdbae80c7585e6233ea82773
EC2 Default User <ec2-user@ip-172-31-1-81.us-west-1.compute.internal>
2020-11-15 04:53:55 +0000

    init commit

---
+++ hello_world/app.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from fake_useragent import UserAgent
+import boto3
+
+class ProductToSearch:
+  def __init__(self, name, url):
+    self.name = name
+    self.url = url
+
+
+def lambda_handler(event, context):
+    ua = UserAgent()
+    sns = boto3.client('sns')
+    header = {'User-Agent':str(ua.chrome)}
+
+    products = [
+        ProductToSearch("AMD Ryzen 7 5800X","https://www.microcenter.com/product/630284/amd-ryzen-7-5800x-vermeer-38ghz-8-core-am4-boxed-processor?storeid=101"),
+        ProductToSearch("AMD Ryzen 5 5600X","https://www.microcenter.com/product/630285/amd-ryzen-5-5600x-vermeer-37ghz-6-core-am4-boxed-processor-with-wraith-stealth-cooler?storeid=101")
+    ]
+    for product in products:
+        req = requests.get(product.url, header)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        inventoryCountSpan=soup.find("span", class_="inventoryCnt")
+    
+        if inventoryCountSpan.getText() != "Sold Out":
+            response = sns.publish(
+            TopicArn='arn:aws:sns:us-west-1:640172007277:ryzen-in-stock',    
+            Message=f'{product.name} in stock at Tustin Microcenter! {product.url}')
+    return 'hi'
