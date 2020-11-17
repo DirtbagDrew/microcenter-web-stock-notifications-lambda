15999bc63443b10310baa6eb4c987f5910a12f83
DirtbagDrew <andrewmyerhb@gmail.com>
2020-11-17 12:12:54 -0800

    add another topic

--- microcenter-stock-scraper/app.py
+++ microcenter-stock-scraper/app.py
@@ -12,17 +12,18 @@ class ProductStockResult:
     return '{"name" : "' + self.name + '", "isInStock": "' + str(self.isInStock) + '"}'
     
 class ProductToSearch:
-  def __init__(self, name, url):
+  def __init__(self, name, url, snsTopicArn):
     self.name = name
     self.url = url
+    self.snsTopicArn=snsTopicArn
 
 ua = UserAgent()
 sns = boto3.client('sns')
 header = {'User-Agent':str(ua.chrome)}
 
 products = [
-    ProductToSearch("AMD Ryzen 7 5800X","https://www.microcenter.com/product/630284/amd-ryzen-7-5800x-vermeer-38ghz-8-core-am4-boxed-processor?storeid=101"),
-    ProductToSearch("AMD Ryzen 5 5600X","https://www.microcenter.com/product/630285/amd-ryzen-5-5600x-vermeer-37ghz-6-core-am4-boxed-processor-with-wraith-stealth-cooler?storeid=101")
+    ProductToSearch("AMD Ryzen 7 5800X","https://www.microcenter.com/product/630284/amd-ryzen-7-5800x-vermeer-38ghz-8-core-am4-boxed-processor?storeid=101","arn:aws:sns:us-west-1:640172007277:ryzen-in-stock"),
+    ProductToSearch("AMD Ryzen 5 5600X","https://www.microcenter.com/product/630285/amd-ryzen-5-5600x-vermeer-37ghz-6-core-am4-boxed-processor-with-wraith-stealth-cooler?storeid=101","arn:aws:sns:us-west-1:640172007277:ryzen-5-in-stock")
 ]
 
 def lambda_handler(event, context):
@@ -36,7 +37,7 @@ def lambda_handler(event, context):
         if inventoryCountSpan.getText() != "Sold Out":
             isInStock = False
             sns.publish(
-            TopicArn='arn:aws:sns:us-west-1:640172007277:ryzen-in-stock',    
+            TopicArn=product.snsTopicArn,    
             Message=f'{product.name} in stock at Tustin Microcenter! {product.url}')
         res.append(ProductStockResult(product.name, isInStock).toJson())
         
